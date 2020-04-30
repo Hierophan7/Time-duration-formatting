0f58462bdc5bb8fe237baa82d616ea0749284135
5P4C3GR33D <cyber.cuttlefish@gmail.com>
2020-05-01 00:38:31 +0300

    Ignoring /obj and /bin, added class with all calculations

---
+++ Task 1/Formatting.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class Formatting
+    {
+        public int formatDuration;
+        public StringBuilder answer;
+
+        public void Calculations()
+        {
+            Console.Write("Enter your time in seconds: ");
+            formatDuration = 0;
+            while (!int.TryParse(Console.ReadLine(), out formatDuration))
+            {
+                Console.Clear();
+                Console.WriteLine("Please, use integer for time display.");
+            }
+            
+            if (formatDuration <= 0)
+            {
+                Console.WriteLine("now");
+                return;
+            }
+
+            int years, days, hours, minutes, seconds;
+            answer = new StringBuilder();
+
+            TimeSpan t = TimeSpan.FromSeconds(formatDuration);
+            years = t.Days / 365;
+            days = t.Days % 365;
+            hours = t.Hours;
+            minutes = t.Minutes;
+            seconds = t.Seconds;
+
+            if (years != 0)
+            {
+                answer.Append(years + ((years == 1) ? " year, " : " years, "));
+            }
+            if (days != 0)
+            {
+                answer.Append(days + ((days == 1) ? " day, " : " days, "));
+            }
+            if (hours != 0)
+            {
+                answer.Append(hours + ((hours == 1) ? " hour, " : " hours, "));
+            }
+            if (minutes != 0)
+            {
+                answer.Append(minutes + ((minutes == 1) ? " minute, " : " minutes, "));
+            }
+            if (seconds != 0)
+            {
+                answer.Append(seconds + ((seconds == 1) ? " second, " : " seconds, "));
+            }
+
+            answer.Length -= 2;
+            int idx = answer.ToString().LastIndexOf(",");
+            if (idx > 0) answer.Remove(idx, 2).Insert(idx, " and ");
+
+            Console.WriteLine(answer);
+        }
+    }
+}
