0e77f25780d4d5064e06e69e3ad55f3bc49b3920
5P4C3GR33D <cyber.cuttlefish@gmail.com>
2020-05-27 14:38:30 +0300

    added desktop interface

---
+++ Desktop interface/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Desktop_interface
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.computeButton = new System.Windows.Forms.Button();
+            this.outputCurrent = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.inputData = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dbOutput = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.computeButton);
+            this.groupBox1.Controls.Add(this.outputCurrent);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.inputData);
+            this.groupBox1.Location = new System.Drawing.Point(25, 31);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(244, 177);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Input";
+            // 
+            // computeButton
+            // 
+            this.computeButton.Location = new System.Drawing.Point(67, 77);
+            this.computeButton.Name = "computeButton";
+            this.computeButton.Size = new System.Drawing.Size(102, 56);
+            this.computeButton.TabIndex = 7;
+            this.computeButton.Text = "Compute";
+            this.computeButton.UseVisualStyleBackColor = true;
+            this.computeButton.Click += new System.EventHandler(this.computeButton_Click);
+            // 
+            // outputCurrent
+            // 
+            this.outputCurrent.Location = new System.Drawing.Point(6, 150);
+            this.outputCurrent.Name = "outputCurrent";
+            this.outputCurrent.ReadOnly = true;
+            this.outputCurrent.Size = new System.Drawing.Size(232, 20);
+            this.outputCurrent.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Enter time in seconds:";
+            // 
+            // inputData
+            // 
+            this.inputData.Location = new System.Drawing.Point(6, 38);
+            this.inputData.Name = "inputData";
+            this.inputData.Size = new System.Drawing.Size(232, 20);
+            this.inputData.TabIndex = 4;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dbOutput);
+            this.groupBox2.Location = new System.Drawing.Point(304, 31);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(262, 177);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Last five items:";
+            // 
+            // dbOutput
+            // 
+            this.dbOutput.Location = new System.Drawing.Point(7, 20);
+            this.dbOutput.Multiline = true;
+            this.dbOutput.Name = "dbOutput";
+            this.dbOutput.ReadOnly = true;
+            this.dbOutput.Size = new System.Drawing.Size(249, 150);
+            this.dbOutput.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 301);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(155, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "©rttsu special for Web-Systems";
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(499, 291);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 4;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(586, 323);
+            this.ControlBox = false;
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Time duration formatting";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox inputData;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox outputCurrent;
+        private System.Windows.Forms.Button computeButton;
+        private System.Windows.Forms.TextBox dbOutput;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ Desktop interface/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TimeDBContext;
+using Calculations;
+
+namespace Desktop_interface
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static int formatDuration;
+        private TimeContext db = new TimeContext();
+
+        protected override void OnLoad(EventArgs e)
+        {
+            var times = db.Times.ToList().Reverse<Time>().Take(5).Reverse();
+            dbOutput.Text = "";
+            foreach (Time unit in times)
+            {
+                dbOutput.Text += unit.Output + "\r\n";
+            }
+        }
+        private void computeButton_Click(object sender, EventArgs e)
+        {
+            formatDuration = 0;
+            if (int.TryParse(inputData.Text, out formatDuration)) {
+                outputCurrent.Text = TimeCalculations.Calculations(formatDuration);
+                db.Times.Add(new Time() { Output = outputCurrent.Text });
+                db.SaveChanges();
+                OnLoad(e);
+            } else {
+                outputCurrent.Text = "The number is too large or wrong input data.";
+            }
+            inputData.Clear();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
